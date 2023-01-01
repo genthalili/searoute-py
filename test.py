3f0d1c707f0bb9f29eda89556c23615bb57fd21d
genthalili <halili.gent@gmail.com>
2023-01-01 17:40:00 +0100

    v1.0.9

--- test.py
+++ test.py
@@ -5,7 +5,8 @@ origin = [21.1545, 55.6526]
 destination = [-118.2629, 33.7276]
 
 
-route = sr.searoute(origin, destination, append_orig_dest=True)
+
+route = sr.searoute(origin, destination, append_orig_dest=True, restrictions=['northwest'])
 # > Returns a GeoJSON LineString Feature
 # show route distance with unit
 print("{:.1f} {}".format(route.properties['length'], route.properties['units']))
