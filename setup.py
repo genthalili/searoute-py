e063d76016270a1482144d76ceb0ab5ec34b45d3
Gent Halili <gent.halili@inter.ikea.com>
2023-01-04 17:43:25 +0100

    v1.0.9

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ classifiers = [
 
 setup(
     name='searoute',
-    version='1.0.8',
+    version='1.0.9',
     description='A python package for generating the shortest sea route between two points on Earth.',
     long_description=open('README.md').read() + '\n\n' +
     open('CHANGELOG.txt').read(),
