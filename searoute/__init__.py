8fcfdfb361fae8a098f146ac5ef1aeadad1f8486
Josh Kirk <59419126+this-josh@users.noreply.github.com>
2024-06-05 12:32:35 +0100

    create M as and P as cached functions otherwise changes are made in place

--- searoute/__init__.py
+++ searoute/__init__.py
@@ -1,3 +1,3 @@
-from .searoute import from_nodes_edges_set, searoute, P, M, marnet, ports
+from .searoute import from_nodes_edges_set, searoute, setup_P, setup_M, marnet, ports
 from .classes.marnet import Marnet
 from .classes.ports import Ports
